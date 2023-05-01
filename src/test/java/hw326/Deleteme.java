a8473fb64e556af1d8bb8343c83e87e1a24bf89d
Malak1993 <127646362+Malak1993@users.noreply.github.com>
2023-05-01 17:36:19 -0500

    new class

---
+++ src/test/java/hw326/Deleteme.java
@@ -0,0 +1,10 @@
+package hw326;
+
+public class Deleteme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("delete me when you done");
+	}
+
+}
